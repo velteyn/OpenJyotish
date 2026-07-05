941fec8937d8d1afd421593ef830533f897bc6ac
Edoardo BAROLO <velteyn@gmail.com>
2026-07-05 09:53:55 +0000

    Start window maximized (full screen on launch)

--- src/jhora/cli/main.py
+++ src/jhora/cli/main.py
@@ -311,7 +311,7 @@ def gui():
     qapp.setApplicationName("the classical tradition")
     qapp.setStyle("Fusion")
     window = MainWindow()
-    window.show()
+    window.showMaximized()
     qapp.exec()
 
 
